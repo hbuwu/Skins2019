c60f4bc352fd40e4078983f8443b99508ec59944
Alex <alexander.vanbets@gmail.com>
2020-01-31 22:48:56 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,63 @@
-//Placeholder
+let divs = document.querySelectorAll("div");
+
+console.log(divs);
+
+divs[1].addEventListener("click", function(){
+    window.location.href = "https://www.mediafire.com/file/pybc0sy2ryxnziq/-_Shige_Seoul_16-9_custom.osk/file";
+})
+
+divs[2].addEventListener("click", function(){
+    window.location.href = "https://drive.google.com/open?id=19KSC7wepmpqxdyKc4tX49LUWR651f1Hf";
+})
+
+divs[3].addEventListener("click", function(){
+    window.location.href = "https://puu.sh/wbUqh/f06e03e683.osk";
+})
+
+divs[4].addEventListener("click", function(){
+    window.location.href = "https://puu.sh/EHuP1/6aeafe0647.osk";
+})
+
+divs[5].addEventListener("click", function(){
+    window.location.href = "";
+})
+
+divs[6].addEventListener("click", function(){
+    window.location.href = "https://files.catbox.moe/0p86px.osk";
+})
+
+divs[7].addEventListener("click", function(){
+    window.location.href = "https://puu.sh/DMQBA/9976a5c484.osk";
+})
+
+divs[8].addEventListener("click", function(){
+    window.location.href = "http://ck1t.ru/s-1107";
+})
+
+divs[9].addEventListener("click", function(){
+    window.location.href = "https://www.reddit.com/r/OsuSkins/comments/butyvk/mikugalaxy_skin_std_onlyhd/";
+})
+
+divs[10].addEventListener("click", function(){
+    window.location.href = "https://mega.nz/#!3jIAwawY!A0hvIUdrZ1lMv5okKJnxTln7zExkx3-C69qFNRJIvzA";
+})
+
+divs[11].addEventListener("click", function(){
+    window.location.href = "https://skins.osuck.net/index.php?newsid=640";
+})
+
+divs[12].addEventListener("click", function(){
+    window.location.href = "https://cdn.discordapp.com/attachments/541342761417310228/654360334643232788/ShelbisMIX.osk";
+})
+
+divs[13].addEventListener("click", function(){
+    window.location.href = "http://puu.sh/EHyN3.osk";
+})
+
+divs[14].addEventListener("click", function(){
+    window.location.href = "https://www.dropbox.com/s/pw6bnlg4o1oaibg/Cherryru.osk?dl=0";
+})
+
+divs[15].addEventListener("click", function(){
+    window.location.href = "https://mega.nz/#F!4lc2jaTY!p3z3WxFdbrVY5kQibRoPlQ";
+})
